07620329d5b40ba02bc5e543614fa89a94a68de3
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 22:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function takeANumber(currentLine, newName){
   var newPlace = currentLine + 1;
   var greeting = `Welcome, ${newName}. You are number ${newPlace} in line.`;
 
-  
+
   return greeting; 
   }
