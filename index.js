168336f009b58340705457bff93e46d2272802b8
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 23:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function nowServing(katzDeliLine){
   }
 
 }
+function currentLine(katzDeliLine){
+ var str = 'The line is currently ';
+  if(!katzDeliLine.length){
+    return 'The line is currently empty';
+  } else {
+    for (var i = 0; i < katzDeliLine.length; i++){
+      str.push([i + 1] + '. ' + katzDeliLine[i]);
+    }
+    return str;
+  }
+}
