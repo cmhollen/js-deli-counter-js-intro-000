f4dcc09adbe2eb16af6a2a1c723d782f4e51d356
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 23:21:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine){
   if (!katzDeliLine.length){
     return'There is nobody waiting to be served!';
   } else {
-    return `Currently serving ${katzDeliLine.shift()}`;
+    return `Currently serving ${katzDeliLine.shift()}.`;
   }
 
 }
