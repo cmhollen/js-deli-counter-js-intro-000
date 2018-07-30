502a921711845bd2c4d3af1c84473a4f9ffc0466
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 22:26:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ function takeANumber(currentLine, newName){
   var katzDeliLine = [];
 
   while(0 < currentLine){
-  
+
     katzDeliLine.push(currentLine++);
   }
   return `Welcome, ${newName}. You are number ${currentLine} in line.`;
   }
-  
