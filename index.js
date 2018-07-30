d41c43e2c5847df311daeee629cc735940498c74
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 22:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function takeANumber(currentLine, newName){
-  
+
   for(var i = 0; i < katzDeliLine.length; i++){
     katzDeliLine.push(currentLine++);
-    
+
   }
   return `Welcome, ${newName}. You are number ${currentLine} in line.`;
 }
