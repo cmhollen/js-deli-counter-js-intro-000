f713161ca018c35c68248c608eec69a18cbdfd8c
cmhollen <cmhollenkamp@gmail.com>
2018-07-30 23:12:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function newServing(katzDeliLine){
   if (!katzDeliLine.length){
     return 'There is nobody waiting to be served!';
   } else {
-    return katzDeliLine.shift();
+    return katzDeliLine[0];
   }
+  katzDeliLine.shift();
+  return;
 }
